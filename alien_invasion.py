5c17fc1ca74a5b23428059aab41e14fddc3cb537
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-12 17:17:00 +0800

    ship fail

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behaviour"""
@@ -9,7 +10,7 @@ class AlienInvasion:
 		"""Initializes the game and creates game resources"""
 		pygame.init()
 		self.settings = Settings()
-
+		self.ship = Ship(self)
 		self.screen = pygame.display.set_mode(
 			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
@@ -25,6 +26,7 @@ class AlienInvasion:
 
 			# Redraw the screen after every loop
 			self.screen.fill(self.settings.bg_color)
+			self.ship.blitme()
 
 			# Make the most recently drawn screen visible
 			pygame.display.flip()
