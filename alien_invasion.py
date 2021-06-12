98a2bfc82912526c8c5ffdc299e164edc62f7ba9
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-12 17:29:17 +0800

    ship image added

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,10 +10,10 @@ class AlienInvasion:
 		"""Initializes the game and creates game resources"""
 		pygame.init()
 		self.settings = Settings()
-		self.ship = Ship(self)
 		self.screen = pygame.display.set_mode(
 			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
+		self.ship = Ship(self)
 
 	def run_game(self):
 		"""Start main loop for the game"""
