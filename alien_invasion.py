f8d05bb7ffc0de01474cc66e7701bac484c7b48a
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-14 15:52:36 +0800

    rows of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,21 +92,28 @@ class AlienInvasion:
 		# Make an alien and find number of aliens in a row
 		# Spacing between each alien is one alien width
 		alien = Alien(self)
-		alien_width = alien.rect.width
+		alien_width, alien_height = alien.rect.size
 		available_space_x = self.settings.screen_width - (alien_width * 2)
 		number_aliens = available_space_x // (alien_width * 2)
 
-		# Create first row of aliens
-		for alien_number in range(number_aliens):
-			# Create in alien and put it in first row
-			self._create_alien(alien_number)
+		# Determine the number of rows that can fit on the screen
+		ship_height = self.ship.rect.height
+		avilable_space_y = (
+			self.settings.screen_height - (3 * alien_height) - ship_height)
+		number_rows = avilable_space_y // (alien_height * 2)
 
-	def _create_alien(self, alien_number):
+		# Create full fleet of aliens
+		for row_number in range(number_rows):
+			for alien_number in range(number_aliens):
+				self._create_alien(alien_number, row_number)
+
+	def _create_alien(self, alien_number, row_number):
 		"""Create an alien and place it in a row"""
 		alien = Alien(self)
-		alien_width = alien.rect.width
+		alien_width, alien_height = alien.rect.size
 		alien.x = alien_width + 2 * alien_width * alien_number
 		alien.rect.x = alien.x
+		alien.rect.y = alien_height + 2 * alien.rect.height * row_number
 		self.aliens.add(alien)
 
 
