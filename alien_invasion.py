10960e21e3e5ab6c9e24a84a25257eb48a0c9e70
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-14 12:19:29 +0800

    bullet refinement

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,7 +26,7 @@ class AlienInvasion:
 
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
+			self._update_bullets()
 			self._update_screen()
 
 	def _check_events(self):
@@ -67,8 +67,21 @@ class AlienInvasion:
 	def _fire_bullet(self):
 		"""Create new bullet and add it to the bullets group"""
 
-		new_bullet = Bullet(self)
-		self.bullets.add(new_bullet)
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+
+	def _update_bullets(self):
+		"""Updates position of bullets and deletes old bullets"""
+
+		# Update bullet position
+		self.bullets.update()
+
+		# Get rid of bullets after they've dissappeared
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+
 
 	def _update_screen(self):
 		"""Updates images on the screen and flips to new screen"""
