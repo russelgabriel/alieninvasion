217b1745dc7b7f4a7ad4d53e65b381bd0a85eb43
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-14 14:01:13 +0800

    refactor create alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,10 +99,15 @@ class AlienInvasion:
 		# Create first row of aliens
 		for alien_number in range(number_aliens):
 			# Create in alien and put it in first row
-			alien = Alien(self)
-			alien.x = alien_width + 2 * alien_width * alien_number
-			alien.rect.x = alien.x
-			self.aliens.add(alien)
+			self._create_alien(alien_number)
+
+	def _create_alien(self, alien_number):
+		"""Create an alien and place it in a row"""
+		alien = Alien(self)
+		alien_width = alien.rect.width
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		self.aliens.add(alien)
 
 
 	def _update_screen(self):
