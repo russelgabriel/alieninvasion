4951f619dab6db6e72496b13779b3b2eeedbbc22
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-18 15:13:56 +0800

    fixing play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,8 +62,22 @@ class AlienInvasion:
 	def _check_play_button(self, mouse_pos):
 		"""Start new game when player clicks play"""
 
-		if self.play_button.rect.collidepoint(mouse_pos):
+		button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+
+		if button_clicked and not self.stats.game_active:
+			# Reset game stats
+			self.stats.reset_stats()
 			self.stats.game_active = True
+
+			# Get rid of remaining aliens and bullets
+			self.aliens.empty()
+			self.bullets.empty()
+
+			# Create new fleet and center ship
+			self._create_fleet()
+			self.ship.center_ship()
+
+
 
 	def _check_keydown_events(self, event):
 		"""Respond to key presses"""
