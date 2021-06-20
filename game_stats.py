af05de8b514a5efd417b45b7478b7dc18f5a90a2
Russel Sy <russelsy@Russels-MacBook-Pro-150.local>
2021-06-20 17:07:23 +0800

    add high score

--- game_stats.py
+++ game_stats.py
@@ -10,6 +10,9 @@ class GameStats:
 		# Start alien invasion in an inactive state
 		self.game_active = False
 
+		# High score should never be reset
+		self.high_score = 0
+
 	def reset_stats(self):
 		"""Initialize statistics that can change during game"""
 		self.ships_left = self.settings.ship_limit
--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,7 @@ class Scoreboard:
 
 		# Prepare initial score image
 		self.prep_score()
+		self.prep_high_score()
 
 	def prep_score(self):
 		"""Turn score into rendered image"""
@@ -29,6 +30,25 @@ class Scoreboard:
 		self.score_rect.right = self.screen_rect.right - 20
 		self.score_rect.top = 20
 
+	def prep_high_score(self):
+		"""Turn high score into rendered image"""
+		high_score = round(self.stats.high_score, -1)
+		high_score_str = '{:,}'.format(high_score)
+		self.high_score_image = self.font.render(
+			high_score_str, True, self.text_color, self.settings.bg_color)
+
+		# Center high score on top of the screen
+		self.high_score_rect = self.high_score_image.get_rect()
+		self.high_score_rect.centerx = self.screen_rect.centerx
+		self.high_score_rect.top = self.screen_rect.top
+
+	def check_high_score(self):
+		"""Check to see if theres a new high score"""
+		if self.stats.score > self.stats.high_score:
+			self.stats.high_score = self.stats.score
+			self.prep_high_score()
+
 	def show_score(self):
 		"""Draw score on the screen"""
 		self.screen.blit(self.score_image, self.score_rect)
+		self.screen.blit(self.high_score_image, self.high_score_rect)
